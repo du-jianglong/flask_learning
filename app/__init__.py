ce401a33af0cc307e0e11201809ba4d38ec6bb7b
dujianglong <dujianglong@foxmail.com>
2018-05-22 14:45:08 +0800

    add image file && edit README.md file

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from flask import Flask     # 从 flask 这个框架中导入 Flask 这个类
 
 app = Flask(__name__)
 '''
-初始化一个 Flask 对象：
+Flask(__name__) 初始化一个 Flask 对象 ：
 1、方便 flask 框架寻找资源
 2、方便 flask 插件，比如： Flask-Sqlalchemy 出现错误的时候，能够找到问题所在位置
 '''
 
-from app import views
+from app import views   # 导入视图模块
 
 
 
