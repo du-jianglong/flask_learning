e3bd8555cf58a1bc89fb31944afd66409b5e634c
dujianglong <dujianglong@foxmail.com>
2018-05-22 18:40:53 +0800

    commit changed file

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,9 @@
-from app import app
+# 导入 Form 类，并导入两个字段类，StringField 和 BooleanField
+# DataRequired 验证器只是简单地检查相应域提交的数据是否是空
+from flask.ext.wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
